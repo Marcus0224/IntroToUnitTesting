070b0aeec69f7b194acf9cce92a9e467ce2fd7e5
Marcus0224 <marcstlouis04@gmail.com>
2023-10-09 21:52:32 -0400

    initial commit

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,15 @@
+/**
+ * The class containing your tests for the {@link Demo} class.  Make sure you
+ * test all methods in this class (including both 
+ * {@link Demo#main(String[])} and 
+ * {@link Demo#isTriangle(double, double, double)}).
+ */
+import java.beans.Transient;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import Demo.java; 
+
+
+}
